d373c2205dee548728aa4607aa99a31a1cbf3016
dosabalint <dosa.balint@gmail.com>
2018-06-14 16:38:44 +0200

    blog oldal feldarabolva

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,19 @@
     <div>
         <Navigation></Navigation>
 
-        <index></index>
+        <!-- temp nav -->
+        <ul>
+            <li @click="currentPage = 'index'">
+                index
+            </li>
+            <li @click="currentPage = 'blog'">
+                blog
+            </li>
+        </ul>
+
+        <keep-alive>
+            <component :is="currentPage"></component>
+        </keep-alive>
 
         <Footer></Footer>
     </div>
@@ -12,12 +24,20 @@
 import Navigation from './components/Navigation.vue';
 import Footer from './components/Footer.vue';
 import index from './pages/index.vue';
+import blog from './pages/blog.vue';
 
 export default {
+    name: 'App',
     components: {
         Navigation,
         Footer,
-        index
+        index,
+        blog
+    },
+    data: function() {
+        return {
+            currentPage: 'index'
+        };
     }
 };
 </script>
---
+++ src/pages/blog.vue
@@ -0,0 +1,244 @@
+<template>
+    <div> 
+
+<div class="jumbotron jumbotron-fluid mb-0">
+  <div class="container">
+    <h1 class="">Mennyit keresnék, ha...?</h1>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fugiat sequi, placeat dolor aliquid porro a quisquam ut,
+      accusantium quasi assumenda maiores ipsum vel.</p>
+    <a class="btn btn-green btn-lg mt-3" href="#" role="button">Tovább a kérdőívekre!</a>
+  </div>
+</div>
+
+<div class="container">
+
+  <div class="row">
+
+    <main class="col-12 col-md-9">
+
+      <div class="row px-lg-5">
+
+        <div class="card-deck mt-5">
+
+          <div class="row">
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep2.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep3.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep4.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep1.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep5.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep6.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep7.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep1.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep1.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-lg-6 my-3">
+              <div class="card blog-card">
+                <img class="card-img-top" src="../img/blog_kep1.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title text-uppercase">Lorem ipsum dolor.</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione at, pariatur! Temporibus enim, nemo architecto.</p>
+                  <p class="card-text">
+                    <small class="text-muted">2018.01.03</small>
+                  </p>
+                  <a href="cikk.html" class="float-right">Tovább olvasom...</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <nav aria-label="Page navigation example" class="col-11 mt-5">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#">Előző</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">1</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">Következő</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </main>
+
+    <aside id="aside" class="col-12 col-md-3 mt-5">
+
+      <!-- Search -->
+      <form class="form-inline">
+        <input class="form-control mr-sm-2 mb-2" type="search" placeholder="Search" aria-label="Keresés">
+        <button class="btn btn-sm btn-outline-success my-2 my-sm-0" type="submit">Keresés</button>
+      </form>
+      <!-- /Search -->
+
+      <div class="mt-5">
+        <h3>Kategóriák</h3>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <a href="#">Kategória1 (27)</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Kategória2 (42)</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Kategória3 (74)</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Kategória4 (6)</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Kategória5 (11)</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /Category -->
+      <div class="my-5">
+        <h3 class="mb-3">Tags</h3>
+        <a href="#" class="badge badge-primary badge-pill p-2 m-1">Informatika</a>
+        <a href="#" class="badge badge-secondary badge-pill p-2 m-1">Marketing</a>
+        <a href="#" class="badge badge-success badge-pill p-2 m-1">Oktatás</a>
+        <a href="#" class="badge badge-danger badge-pill p-2 m-1">Művészet</a>
+        <a href="#" class="badge badge-warning badge-pill p-2 m-1">Warning</a>
+        <a href="#" class="badge badge-info badge-pill p-2 m-1">Info</a>
+        <a href="#" class="badge badge-light badge-pill p-2 m-1">Light</a>
+        <a href="#" class="badge badge-dark badge-pill p-2 m-1">Dark</a>
+      </div>
+
+    </aside>
+
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
