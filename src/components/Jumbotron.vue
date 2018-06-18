52257c0994940156d20d0662e2d99b56fd315d48
dosabalint <dosa.balint@gmail.com>
2018-06-18 02:53:13 +0200

    Jumbotron, YoutubeEmbed komponensek kész

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="jumbotron jumbotron-fluid mb-0">
+        <div class="container">
+            <h1>
+                <slot name="title"></slot>
+            </h1>
+            <p class="lead">
+                <slot name="lead"></slot>
+            </p>
+            <a class="btn btn-green btn-lg mt-3" :href="buttonHref" role="button">{{ buttonText }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['buttonHref', 'buttonText']
+};
+</script>
---
+++ src/components/YoutubeEmbedSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md-8 mx-auto">
+                <h3 class="mb-4">
+                    {{ title }}
+                </h3>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" :src="src" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['title', 'src']
+};
+</script>
+
