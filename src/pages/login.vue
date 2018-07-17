f8068e99fed4cb2a5cb4e55c7081fec0390a1403
Gergo Nagy <gergonagy@Gergos-MacBook-Pro.local>
2018-07-17 18:16:32 +0200

    registration and login page refactor done

---
+++ src/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="bejelentkezes">
+<div class="container">
+
+    <div class="row my-auto">
+      <div class="col-12 col-lg-6 mx-auto">
+        <div class="card login-card mb-3">
+          <div class="card-header login-card-header text-uppercase">Bejelentkezés</div>
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Add meg az email címed</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="valami@email.com">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Add meg a jelszavad</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Jelszó">
+                <small class="form-text"><a href="#">Elfelejtetted a jelszavad?</a></small>
+                <div class="form-check mt-3">
+                  <input class="form-check-input" type="checkbox" id="autoSizingCheck2">
+                  <label class="form-check-label" for="autoSizingCheck2">Bejelentkezve maradok</label>
+                </div>
+                <div class="my-3 text-center">
+                  <button type="submit" class="btn loginBtn loginBtn--facebook py-2 my-2"><img src="img/facebook.svg" height="20" alt="" class="mr-2 mb-1">Facebook bejelentkezés</button>
+                  <button type="submit" class="btn loginBtn loginBtn--google py-2 my-2"><img src="img/google.svg" height="25" alt="" class="mr-2">Google bejelentkezés</button>
+                </div>
+                <div class="card-footer"></div>
+                <a class="btn btn-login btn-lg btn-block text-uppercase" href="index.html" role="button">Bejelentkezés</a>
+                <p class="pt-3 small"><a href="regisztracio.html">Még nem regisztáltál?</a></p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>  
+</template>
+
+<script>
+export default {
+  name: "login"
+};
+</script>
+
---
+++ src/pages/registration.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="regisztracio">
+    <div class="container">
+
+      <div class="row margin">
+      <div class="col-6 mx-auto">
+
+        <div class="card login-card mb-3">
+          <div class="card-header h3">Regisztrácó</div>
+          <div class="card-body">
+
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Felhasználónév</label>
+                <input type="text" class="form-control" placeholder="Felhasználónév">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Jelszó</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Jelszó még egyszer</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+
+
+
+              <div class="my-3 text-center">
+                <button type="submit" class="btn loginBtn loginBtn--facebook py-2"><i class="fa fa-facebook pr-2"></i>Facebook bejelentkezés</button>
+                <button type="submit" class="btn loginBtn loginBtn--google py-2"><i class="fa fa-google pr-2"></i>Google bejelentkezés</button>
+              </div>
+              <div class="card-footer bg-transparent"></div>
+              <a class="btn btn-primary btn-lg btn-block" href="index.html" role="button">Regisztráció</a>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "registration"
+};
+</script>
+
