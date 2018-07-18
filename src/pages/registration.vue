23ba05b3376ca1cce8a1bdeb0dee15c67b6781e2
Nagy Gergo <nagy.gergo@3pixel.hu>
2018-07-18 18:26:14 +0200

    alap registration bekotes

--- src/pages/registration.vue
+++ src/pages/registration.vue
@@ -8,24 +8,28 @@
         <div class="card login-card mb-3">
           <div class="card-header h3">Regisztrácó</div>
           <div class="card-body">
-
-            <form>
-              <div class="form-group">
+            <div class="col-12 alert alert-danger" role="alert" v-if="alerts.length">
+              <ul>
+                <li v-for="a in alerts" :key="a">{{a}}</li>
+              </ul>
+            </div>
+            <form @submit.prevent="registration()">
+              <!-- <div class="form-group">
                 <label for="exampleInputEmail1">Felhasználónév</label>
                 <input type="text" class="form-control" placeholder="Felhasználónév">
-              </div>
+              </div> -->
               <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Jelszó</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Jelszó még egyszer</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <input v-model="password2" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
               </div>
 
 
@@ -35,21 +39,39 @@
                 <button type="submit" class="btn loginBtn loginBtn--google py-2"><i class="fa fa-google pr-2"></i>Google bejelentkezés</button>
               </div> -->
               <div class="card-footer bg-transparent"></div>
-              <a class="btn btn-primary btn-lg btn-block" href="index.html" role="button">Regisztráció</a>
+              <button class="btn btn-primary btn-lg btn-block"
+                type="submit"
+              >
+                Regisztráció
+              </button>
             </form>
 
           </div>
         </div>
       </div>
     </div>
-
   </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "registration"
+  name: "registration",
+  data() {
+    return {
+      email: "",
+      password: "",
+      password2: "",
+      alerts: []
+    };
+  },
+  methods: {
+    registration() {
+      if (this.password !== this.password2) {
+        this.alerts.push("A ket jelszo nem egyezik, kerlek javitsd!");
+      }
+    }
+  }
 };
 </script>
 
