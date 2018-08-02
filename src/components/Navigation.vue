8132eb3c36063d188f52534c90e0c88f00c8673c
Nagy Gergo <nagy.gergo@3pixel.hu>
2018-08-02 18:30:38 +0200

    navigation refactor

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,36 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0 sticky-top">
-    <router-link to="/" class="navbar-brand fizu">fizu.hu</router-link>
-    
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <router-link to="/"
+                 class="navbar-brand fizu">fizu.hu</router-link>
+
+    <button class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse"
+         id="navbarNav">
       <ul class="navbar-nav ml-auto">
-        
-        <li class="nav-item px-3" v-for="item in itemCollection" :key="item.id">
-          <router-link 
-            :to="item.path" 
-            class="nav-link"
-            data-toggle="collapse" data-target="#navbarNav"> 
+
+        <li class="nav-item px-3"
+            v-for="item in itemCollection"
+            :key="item.id">
+          <router-link :to="item.path"
+                       class="nav-link"
+                       data-toggle="collapse"
+                       data-target="#navbarNav">
 
             {{ item.name }}
           </router-link>
+        </li>
+        <li class="nav-item px-3"
+            v-if="this.isLoggedIn">
+          <a class="nav-link">Logout</a>
         </li>
 
         <!-- <li class="nav-item px-3">
@@ -24,7 +38,7 @@
             Kedvenc statisztikáink
           </a>
         </li> -->
-        
+
         <!-- <li class="nav-item px-3">
           <a class="nav-link" href="kerdoivek.html">
             Kérdőívek
@@ -53,13 +67,69 @@
 </template>
 
 <script>
-import navItems from '../navItems';
+import { mapGetters } from "vuex";
+import { TYPES } from "../store";
 
 export default {
-    data() {
-        return {
-            itemCollection: navItems
-        };
+  computed: {
+    ...mapGetters({ isLoggedIn: TYPES.getters.isLoggedIn }),
+    itemCollection() {
+      if (this.isLoggedIn) {
+        return [
+          ...this.baseNav,
+          {
+            name: "Profile",
+            id: "profile",
+            path: "/profile"
+          }
+        ];
+      } else {
+        return [
+          ...this.baseNav,
+          {
+            name: "Login",
+            id: "login",
+            path: "/login"
+          },
+          {
+            name: "Sign Up",
+            id: "registration",
+            path: "/registration"
+          }
+        ];
+      }
     }
+  },
+  data() {
+    return {
+      baseNav: [
+        {
+          name: "Főoldal",
+          id: "index",
+          path: "/"
+        },
+        {
+          name: "Blog",
+          id: "blog",
+          path: "/blog"
+        },
+        {
+          name: "Kérdőív",
+          id: "survey",
+          path: "/survey"
+        },
+        {
+          name: "Fizutérkép",
+          id: "statistics",
+          path: "/statistics"
+        },
+        {
+          name: "Kapcsolat",
+          id: "kapcsolat",
+          path: "/contact"
+        }
+      ]
+    };
+  }
 };
 </script>
